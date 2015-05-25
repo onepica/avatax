42cb29d85ed833e0670cca021b2280494b80a4da
Rostislav Redko <r.redko@sysiq.com>
2015-05-25 19:04:37 +0200

    Refactored AVAIP-165: Code audit and refactoring - Update code to fit code standards

--- app/code/community/OnePica/AvaTax/Model/Records/Mysql4/Log.php
+++ app/code/community/OnePica/AvaTax/Model/Records/Mysql4/Log.php
@@ -15,22 +15,33 @@
  * @license    http://opensource.org/licenses/osl-3.0.php Open Software License (OSL 3.0)
  */
 
-
+/**
+ * Log resource model
+ *
+ * @category   OnePica
+ * @package    OnePica_AvaTax
+ * @author     OnePica Codemaster <codemaster@onepica.com>
+ */
 class OnePica_AvaTax_Model_Records_Mysql4_Log extends Mage_Core_Model_Mysql4_Abstract
 {
-    protected function _construct() {
+    /**
+     * Construct
+     */
+    protected function _construct()
+    {
         $this->_init('avatax_records/log', 'log_id');
     }
-    
+
     /**
      * Sets various dates before the model is saved.
      *
      * @param Mage_Core_Model_Abstract $object
-     * @return OnePica_AvaTax_Model_Mysql4_Log
+     * @return $this
      */
-    protected function _beforeSave(Mage_Core_Model_Abstract $object) {
+    protected function _beforeSave(Mage_Core_Model_Abstract $object)
+    {
         $object->setCreatedAt(gmdate('Y-m-d H:i:s'));
-        if(!$object->getLevel()) {
+        if (!$object->getLevel()) {
             $object->setLevel('Unknown');
         }
         return $this;
