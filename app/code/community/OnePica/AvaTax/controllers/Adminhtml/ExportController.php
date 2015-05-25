54a2dd21a59a2e9604df2ad39fb1706acde57ea1
Rostislav Redko <r.redko@sysiq.com>
2015-05-25 15:20:22 +0200

    Refactored AVAIP-165: Code audit and refactoring - Update code to fit code standards

--- app/code/community/OnePica/AvaTax/controllers/Adminhtml/ExportController.php
+++ app/code/community/OnePica/AvaTax/controllers/Adminhtml/ExportController.php
@@ -15,31 +15,58 @@
  * @license    http://opensource.org/licenses/osl-3.0.php Open Software License (OSL 3.0)
  */
 
+/**
+ * Admin export controller
+ *
+ * @category   OnePica
+ * @package    OnePica_AvaTax
+ * @author     OnePica Codemaster <codemaster@onepica.com>
+ */
 class OnePica_AvaTax_Adminhtml_ExportController extends Mage_Adminhtml_Controller_Action
 {
+    /**
+     * Check if is allowed
+     *
+     * @return bool
+     */
     protected function _isAllowed()
     {
         return Mage::getSingleton('admin/session')->isAllowed('avatax');
     }
 
+    /**
+     * Log action
+     */
     public function logAction() {
         $fileName   = 'avatax-log-' . gmdate('U') . '.sql';
         $content    = $this->getLayout()->createBlock('avatax/adminhtml_export_log_grid')->getSql();
         $this->_sendResponse($fileName, $content);
     }
-    
-    public function queueAction() {
+
+    /**
+     * Queue action
+     */
+    public function queueAction()
+    {
         $fileName   = 'avatax-queue-' . gmdate('U') . '.sql';
         $content    = $this->getLayout()->createBlock('avatax/adminhtml_export_queue_grid')->getSql();
         $this->_sendResponse($fileName, $content);
     }
 
-    protected function _sendResponse($fileName, $content, $contentType='application/octet-stream') {
+    /**
+     * Send response
+     *
+     * @param string $fileName
+     * @param string $content
+     * @param string $contentType
+     */
+    protected function _sendResponse($fileName, $content, $contentType = 'application/octet-stream')
+    {
         $response = $this->getResponse();
-        $response->setHeader('HTTP/1.1 200 OK','');
+        $response->setHeader('HTTP/1.1 200 OK', '');
         $response->setHeader('Pragma', 'public', true);
         $response->setHeader('Cache-Control', 'must-revalidate, post-check=0, pre-check=0', true);
-        $response->setHeader('Content-Disposition', 'attachment; filename='.$fileName);
+        $response->setHeader('Content-Disposition', 'attachment; filename=' . $fileName);
         $response->setHeader('Last-Modified', date('r'));
         $response->setHeader('Accept-Ranges', 'bytes');
         $response->setHeader('Content-Length', strlen($content));
@@ -48,5 +75,4 @@ class OnePica_AvaTax_Adminhtml_ExportController extends Mage_Adminhtml_Controlle
         $response->sendResponse();
         exit;
     }
-
 }
