d9f3608eb7890db8dbb9a82105af8e880c1b964f
Rostislav Redko <r.redko@sysiq.com>
2015-05-26 14:11:14 +0200

    Refactored AVAIP-165: Code audit and refactoring - Update code to fit code standards

--- app/code/community/OnePica/AvaTax/Model/Source/Customercodeformat.php
+++ app/code/community/OnePica/AvaTax/Model/Source/Customercodeformat.php
@@ -15,12 +15,29 @@
  * @license    http://opensource.org/licenses/osl-3.0.php Open Software License (OSL 3.0)
  */
 
-
+/**
+ * Customer code format source model
+ *
+ * @category   OnePica
+ * @package    OnePica_AvaTax
+ * @author     OnePica Codemaster <codemaster@onepica.com>
+ */
 class OnePica_AvaTax_Model_Source_Customercodeformat
 {
-    const LEGACY        = 0; //depricated, included for stores who are upgrading so the code format doesn't change unexpectedly
-    const CUST_ID        = 1; //recommended
-    const CUST_EMAIL    = 2;
+    /**
+     * Legacy format
+     */
+    const LEGACY = 0; //depricated, included for stores who are upgrading so the code format doesn't change unexpectedly
+
+    /**
+     * Customer id format
+     */
+    const CUST_ID = 1; //recommended
+
+    /**
+     * Customer email format
+     */
+    const CUST_EMAIL = 2;
 
     /**
      * Gets the list of cache methods for the admin config dropdown
