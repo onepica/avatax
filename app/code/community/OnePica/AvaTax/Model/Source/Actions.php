cbe5f2ae7db4f159324349f6cda7d9098335d657
Rostislav Redko <r.redko@sysiq.com>
2015-05-26 14:08:55 +0200

    Refactored AVAIP-165: Code audit and refactoring - Update code to fit code standards

--- app/code/community/OnePica/AvaTax/Model/Source/Actions.php
+++ app/code/community/OnePica/AvaTax/Model/Source/Actions.php
@@ -15,7 +15,13 @@
  * @license    http://opensource.org/licenses/osl-3.0.php Open Software License (OSL 3.0)
  */
 
-
+/**
+ * Actions source model
+ *
+ * @category   OnePica
+ * @package    OnePica_AvaTax
+ * @author     OnePica Codemaster <codemaster@onepica.com>
+ */
 class OnePica_AvaTax_Model_Source_Actions
 {
     /**
