7a91ec7c4f372b2b95fbbb80b5ee0a62b437f02d
krifollk <krifollk@gmail.com>
2015-06-30 22:23:16 +0300

    Implemented AV-550: AvaTax Action Log table. Types of Logs that are not selected in AvaTax Configuration are not displayed in Log table - fixed small mistake

--- app/code/community/OnePica/AvaTax/Helper/Data.php
+++ app/code/community/OnePica/AvaTax/Helper/Data.php
@@ -564,11 +564,11 @@ class OnePica_AvaTax_Helper_Data extends Mage_Core_Helper_Abstract
     public function getExistingTypesLog()
     {
         return array(
-            OnePica_AvaTax_Model_Source_Logtype::FILTER,
-            OnePica_AvaTax_Model_Source_Logtype::GET_TAX,
-            OnePica_AvaTax_Model_Source_Logtype::PING,
-            OnePica_AvaTax_Model_Source_Logtype::QUEUE,
-            OnePica_AvaTax_Model_Source_Logtype::VALIDATE,
+            OnePica_AvaTax_Model_Source_Logtype::FILTER   => OnePica_AvaTax_Model_Source_Logtype::FILTER,
+            OnePica_AvaTax_Model_Source_Logtype::GET_TAX  => OnePica_AvaTax_Model_Source_Logtype::GET_TAX,
+            OnePica_AvaTax_Model_Source_Logtype::PING     => OnePica_AvaTax_Model_Source_Logtype::PING,
+            OnePica_AvaTax_Model_Source_Logtype::QUEUE    => OnePica_AvaTax_Model_Source_Logtype::QUEUE,
+            OnePica_AvaTax_Model_Source_Logtype::VALIDATE => OnePica_AvaTax_Model_Source_Logtype::VALIDATE,
         );
     }
 }
