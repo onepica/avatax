4370f31189626f7f0174d0196fe6b8a5e0bf1dc9
Rostyslav Tymoshenko <krifollk@gmail.com>
2015-07-09 13:36:08 +0300

    Implemented AV-23: Global additon - added new avalara lib (cherry picked from commit d70a340)

---
+++ app/code/community/OnePica/AvaTax/lib/classes/PostTaxResult.class.php
@@ -0,0 +1,58 @@
+<?php
+/**
+ * PostTaxResult.class.php
+ */
+
+/**
+ * Result data returned from {@link TaxServiceSoap#postTax}.
+ *
+ * @see PostTaxRequest
+ * 
+ * @author    Avalara
+ * @copyright © 2004 - 2011 Avalara, Inc.  All rights reserved.
+ * @package   Tax
+ */
+
+class PostTaxResult //extends BaseResult
+{
+  
+		
+// BaseResult innards - work around a bug in SoapClient
+
+/**
+ * @var string
+ */
+    private $TransactionId;
+	
+	private $DocId;
+/**
+ * @var string must be one of the values defined in {@link SeverityLevel}.
+ */
+    private $ResultCode = 'Success';
+/**
+ * @var array of Message.
+ */
+    private $Messages = array();
+
+/**
+ * Accessor
+ * @return string
+ */
+    public function getTransactionId() { return $this->TransactionId; }
+	
+	public function getDocId() { return $this->DocId; }
+/**
+ * Accessor
+ * @return string
+ */
+    public function getResultCode() { return $this->ResultCode; }
+/**
+ * Accessor
+ * @return array
+ */
+    public function getMessages() { return EnsureIsArray($this->Messages->Message); }
+
+
+}
+
+?>
