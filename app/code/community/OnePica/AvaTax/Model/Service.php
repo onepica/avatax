a944ffdb06d15269b6693a35e61e84d3fc850d31
Andrey Zabara <zabara@itdelight.com>
2015-10-19 14:12:44 +0300

    AVTNG-57 Implement service factory - refactor factory

--- app/code/community/OnePica/AvaTax/Model/Service.php
+++ app/code/community/OnePica/AvaTax/Model/Service.php
@@ -26,48 +26,23 @@
 
 class OnePica_AvaTax_Model_Service extends Mage_Core_Model_Factory
 {
-    /** @var null|OnePica_AvaTax_Model_Service_Configuration */
-    protected $_serviceConfig = null;
-
-    /**
-     * OnePica_AvaTax_Model_Factory constructor.
-     */
-    public function __construct()
-    {
-        parent::__construct();
-        $this->setServiceConfig($this->getSingleton('avatax/service_configuration'));
-    }
-
-    /**
-     * @return null|OnePica_AvaTax_Model_Service_Configuration
-     */
-    public function getServiceConfig()
-    {
-        return $this->_serviceConfig;
-    }
-
-    /**
-     * @param null|OnePica_AvaTax_Model_Service_Configuration $serviceConfig
-     *
-     * @return $this
-     */
-    public function setServiceConfig($serviceConfig)
-    {
-        $this->_serviceConfig = $serviceConfig;
-        return $this;
-    }
 
     /**
      * Get factory method
      *
+     * @param $service
+     * @param array $options
      * @return Mage_Core_Model_Abstract|null|OnePica_AvaTax_Model_Service_Abstract
+     * @throws OnePica_SocialPost_Model_Api_Exception
      */
-    public function factory()
+    public function factory($service, array $options = array())
     {
-        try {
-            $model = $this->getModel('avatax/service_' . strtolower($this->getServiceConfig()->getActiveService()));
-        } catch (Exception $e) {
-            Mage::logException($e);
+        if (!$service) {
+            return null;
+        }
+        $model = Mage::getModel('avatax/service_' . $service, $options);
+        if (!$model) {
+            throw new OnePica_SocialPost_Model_Api_Exception("Could not found service Service model '$service'.");
         }
         return $model;
     }
