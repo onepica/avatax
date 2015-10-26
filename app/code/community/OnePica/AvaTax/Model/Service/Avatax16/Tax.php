b18f5304d7ece4151536d6d8149e2a9a0dbc580d
Anatolii Khoma <a.khoma@astoundcommerce.com>
2015-10-26 12:31:28 +0200

    Implemented AVTNG-67: Implement tax calculation (estimation) functionality -base file structure

---
+++ app/code/community/OnePica/AvaTax/Model/Service/Avatax16/Tax.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * OnePica_AvaTax
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Open Software License (OSL 3.0), a
+ * copy of which is available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/osl-3.0.php
+ *
+ * @category   OnePica
+ * @package    OnePica_AvaTax
+ * @author     OnePica Codemaster <codemaster@onepica.com>
+ * @copyright  Copyright (c) 2015 One Pica, Inc.
+ * @license    http://opensource.org/licenses/osl-3.0.php Open Software License (OSL 3.0)
+ */
+
+/**
+ * Avatax16 service tax model
+ *
+ * @category   OnePica
+ * @package    OnePica_AvaTax
+ * @author     OnePica Codemaster <codemaster@onepica.com>
+ */
+class OnePica_AvaTax_Model_Service_Avatax16_Tax extends OnePica_AvaTax_Model_Service_Avatax16_Abstract
+{
+    /**
+     * The document request data object
+     *
+     * @var OnePica_AvaTax16_Document_Request
+     */
+    protected $_request = null;
+
+    /**
+     * An array of line items
+     *
+     * @var array
+     */
+    protected $_lines = array();
+}
