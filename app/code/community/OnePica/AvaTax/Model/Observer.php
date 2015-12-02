cd71401b556b0d6d92a73fa0071171663857b8b8
Anatolii Khoma <a.khoma@astoundcommerce.com>
2015-12-02 17:34:01 +0200

    Implemented AV-859: Refactor Observer Class. Move Each Observer method to separate class -checkoutTypeMultishippingCreateOrdersSingle method

--- app/code/community/OnePica/AvaTax/Model/Observer.php
+++ app/code/community/OnePica/AvaTax/Model/Observer.php
@@ -25,22 +25,6 @@
 class OnePica_AvaTax_Model_Observer extends OnePica_AvaTax_Model_Observer_Abstract
 {
     /**
-     * Stop order creation if tax estimation has problems when multishipping
-     *
-     * @param Varien_Event_Observer $observer
-     * @return $this
-     * @throws OnePica_AvaTax_Exception
-     */
-    public function checkoutTypeMultishippingCreateOrdersSingle(Varien_Event_Observer $observer)
-    {
-        /** @var Mage_Sales_Model_Quote_Address $address */
-        $address = $observer->getEvent()->getAddress();
-        $quote = $address->getQuote();
-        $this->_handleTaxEstimationOnOrderPlace($quote);
-        return $this;
-    }
-
-    /**
      * Delete validation notices on successful order place on multiple checkout
      *
      * @param Varien_Event_Observer $observer
