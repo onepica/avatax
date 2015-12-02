c87d88735b14a525058022ebad800f6f6acb567f
Anatolii Khoma <a.khoma@astoundcommerce.com>
2015-12-02 17:46:36 +0200

    Implemented AV-859: Refactor Observer Class. Move Each Observer method to separate class -salesModelServiceQuoteSubmitAfter method

--- app/code/community/OnePica/AvaTax/Model/Observer/SalesModelServiceQuoteSubmitAfter.php
+++ app/code/community/OnePica/AvaTax/Model/Observer/SalesModelServiceQuoteSubmitAfter.php
@@ -11,17 +11,28 @@
  * @category   OnePica
  * @package    OnePica_AvaTax
  * @author     OnePica Codemaster <codemaster@onepica.com>
- * @copyright  Copyright (c) 2009 One Pica, Inc.
+ * @copyright  Copyright (c) 2015 One Pica, Inc.
  * @license    http://opensource.org/licenses/osl-3.0.php Open Software License (OSL 3.0)
  */
 
 /**
- * Avatax Observer
+ * Avatax Observer SalesModelServiceQuoteSubmitAfter
  *
  * @category   OnePica
  * @package    OnePica_AvaTax
  * @author     OnePica Codemaster <codemaster@onepica.com>
  */
-class OnePica_AvaTax_Model_Observer extends OnePica_AvaTax_Model_Observer_Abstract
+class OnePica_AvaTax_Model_Observer_SalesModelServiceQuoteSubmitAfter extends OnePica_AvaTax_Model_Observer_Abstract
 {
+    /**
+     * Delete validation notices on successful order place
+     *
+     * @param Varien_Event_Observer $observer
+     * @return $this
+     */
+    public function execute(Varien_Event_Observer $observer)
+    {
+        $this->_deleteValidateNotices();
+        return $this;
+    }
 }
