a5abf40daa2b246c0bc6a3bac1f7a7f61ab8af8e
Rostyslav Tymoshenko <krifollk@gmail.com>
2016-02-12 18:50:31 +0200

    Implemented AV-1322: Build admin ui according requirements, develop methods in config classes - Added rewrites for price display setting - If 'catalog prices' set to 'including tax' 'cross border trade' setting will alway return true

--- app/code/community/OnePica/AvaTax/Model/Tax/Config.php
+++ app/code/community/OnePica/AvaTax/Model/Tax/Config.php
@@ -41,6 +41,67 @@ class OnePica_AvaTax_Model_Tax_Config extends Mage_Tax_Model_Config
     }
 
     /**
+     * Get product price display type
+     *
+     *  1 - Excluding tax
+     *  2 - Including tax
+     *  3 - Both
+     *
+     * @param Mage_Core_Model_Store|int $store
+     * @return  int
+     */
+    public function getPriceDisplayType($store = null)
+    {
+        if ($this->_getDataHelper()->isServiceEnabled($store) && $this->_getTaxDataHelper()->priceIncludesTax($store)) {
+            return Mage_Tax_Model_Config::DISPLAY_TYPE_INCLUDING_TAX;
+        }
+
+        return (int)$this->_getStoreConfig(self::CONFIG_XML_PATH_PRICE_DISPLAY_TYPE, $store);
+    }
+
+    /**
+     * Get shipping methods prices display type
+     *
+     * @param Mage_Core_Model_Store|int $store
+     * @return  int
+     */
+    public function getShippingPriceDisplayType($store = null)
+    {
+        if ($this->_getDataHelper()->isServiceEnabled($store)
+            && $this->_getTaxDataHelper()->shippingPriceIncludesTax($store)
+        ) {
+            return Mage_Tax_Model_Config::DISPLAY_TYPE_INCLUDING_TAX;
+        }
+
+        return (int)$this->_getStoreConfig(self::CONFIG_XML_PATH_DISPLAY_SHIPPING, $store);
+    }
+
+    /**
+     * Return the config value for self::CONFIG_XML_PATH_CROSS_BORDER_TRADE_ENABLED
+     *
+     * @param Mage_Core_Model_Store|int $store
+     * @return int
+     */
+    public function crossBorderTradeEnabled($store = null)
+    {
+        if ($this->_getDataHelper()->isServiceEnabled($store) && $this->_getTaxDataHelper()->priceIncludesTax($store)) {
+            return true;
+        }
+
+        return $this->_getStoreConfig(self::CONFIG_XML_PATH_CROSS_BORDER_TRADE_ENABLED, $store);
+    }
+
+    /**
+     * Get tax data helper
+     *
+     * @return Mage_Tax_Helper_Data
+     */
+    protected function _getTaxDataHelper()
+    {
+        return Mage::helper('tax');
+    }
+
+    /**
      * Get avatax data helper
      *
      * @return OnePica_AvaTax_Helper_Data
