05577523c590e9544001ec3c207045a5930bf2dc
Rostyslav Tymoshenko <krifollk@gmail.com>
2016-02-15 18:42:39 +0200

    Implemented AV-1323: implement our Catalog Prices settings in the cart - Added Calculation model rewrite

---
+++ app/code/community/OnePica/AvaTax/Model/Tax/Calculation.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * OnePica_AvaTax
+ * NOTICE OF LICENSE
+ * This source file is subject to the Open Software License (OSL 3.0), a
+ * copy of which is available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/osl-3.0.php
+ *
+ * @category   OnePica
+ * @package    OnePica_AvaTax
+ * @author     OnePica Codemaster <codemaster@onepica.com>
+ * @copyright  Copyright (c) 2016 One Pica, Inc.
+ * @license    http://opensource.org/licenses/osl-3.0.php Open Software License (OSL 3.0)
+ */
+
+/**
+ * Class OnePica_AvaTax_Model_Tax_Calculation
+ */
+class OnePica_AvaTax_Model_Tax_Calculation extends Mage_Tax_Model_Calculation
+{
+    /**
+     * Get calculation tax rate by specific request
+     *
+     * @param   Varien_Object $request
+     * @return  float
+     */
+    public function getRate($request)
+    {
+        if ($this->_getDataHelper()->isServiceEnabled($request->getStore()->getStoreId())) {
+            return 0;
+        }
+
+        return parent::getRate($request);
+    }
+
+    /**
+     * Get avatax data helper
+     *
+     * @return OnePica_AvaTax_Helper_Data
+     */
+    protected function _getDataHelper()
+    {
+        return Mage::helper('avatax');
+    }
+}
