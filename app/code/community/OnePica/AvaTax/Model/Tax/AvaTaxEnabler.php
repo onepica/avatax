88c3175e2ffb5701cf743f7bd0445893ee6f74ea
Anatolii Khoma <a.khoma@astoundcommerce.com>
2016-10-13 10:44:00 +0300

    Implemented AV-1541: Investigate AVAIP-673 and adapt extension

---
+++ app/code/community/OnePica/AvaTax/Model/Tax/AvaTaxEnabler.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * OnePica_AvaTax
+ * NOTICE OF LICENSE
+ * This source file is subject to the Open Software License (OSL 3.0), a
+ * copy of which is available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/osl-3.0.php
+ *
+ * @category   OnePica
+ * @package    OnePica_AvaTax
+ * @author     OnePica Codemaster <codemaster@onepica.com>
+ * @copyright  Copyright (c) 2016 One Pica, Inc.
+ * @license    http://opensource.org/licenses/osl-3.0.php Open Software License (OSL 3.0)
+ */
+
+/**
+ * Singleton Class OnePica_AvaTax_Model_Tax_AvaTaxEnabler
+ */
+class OnePica_AvaTax_Model_Tax_AvaTaxEnabler
+{
+    /**
+     * Is Switched flag. Shows if initTaxCollector method was executed.
+     *
+     * @var bool
+     */
+    protected $_isSwitched = false;
+
+    /**
+     * Init Tax Collector
+     *
+     * @param   int $storeId
+     * @return  $this
+     */
+    public function initTaxCollector($storeId)
+    {
+        if (!$this->_isSwitched) {
+            if (Mage::getStoreConfig('tax/avatax/action', $storeId) != OnePica_AvaTax_Model_Service_Abstract_Config::ACTION_DISABLE) {
+                Mage::getConfig()->setNode('global/sales/quote/totals/tax/class', 'avatax/sales_quote_address_total_tax');
+            }
+            $this->_isSwitched = true;
+        }
+
+        return $this;
+    }
+}
