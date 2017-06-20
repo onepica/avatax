a3960c2351b9107140982b7743fc3bcae9282676
ASTOUND\p.zamoroka <p.zamoroka@astoundcommerce.com>
2017-06-20 16:10:20 +0300

    Added cols to log table

---
+++ app/code/community/OnePica/AvaTax/sql/avatax_records_setup/mysql4-upgrade-3.5.0.0-3.5.0.1.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * OnePica_AvaTax
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Open Software License (OSL 3.0), a
+ * copy of which is available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/osl-3.0.php
+ *
+ * @category   OnePica
+ * @package    OnePica_AvaTax
+ * @author     OnePica Codemaster <codemaster@onepica.com>
+ * @copyright  Copyright (c) 2015 One Pica, Inc.
+ * @license    http://opensource.org/licenses/osl-3.0.php Open Software License (OSL 3.0)
+ */
+
+/** @var $this Mage_Core_Model_Resource_Setup */
+
+$installer = $this;
+$installer->startSetup();
+
+$installer->getConnection()
+    ->addColumn($installer->getTable('avatax_records/log'), 'soap_request', array(
+        'type'     => Varien_Db_Ddl_Table::TYPE_TEXT,
+        'nullable' => false,
+        'after'    => null,
+        'comment'  => 'SOAP request'
+    ));
+
+$installer->getConnection()
+    ->addColumn($installer->getTable('avatax_records/log'), 'soap_result', array(
+        'type'     => Varien_Db_Ddl_Table::TYPE_TEXT,
+        'nullable' => false,
+        'after'    => null,
+        'comment'  => 'SOAP result'
+    ));
+
+$installer->endSetup();
