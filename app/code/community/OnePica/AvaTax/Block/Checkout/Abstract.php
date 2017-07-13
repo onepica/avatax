f0b02babbfb29e4b56884d3927dd4cf199cf131b
ASTOUND\p.zamoroka <p.zamoroka@astoundcommerce.com>
2017-07-13 15:31:37 +0300

    onepage checkout

---
+++ app/code/community/OnePica/AvaTax/Block/Checkout/Abstract.php
@@ -0,0 +1,96 @@
+<?php
+/**
+ * OnePica_AvaTax
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Open Software License (OSL 3.0), a
+ * copy of which is available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/osl-3.0.php
+ *
+ * @category   OnePica
+ * @package    OnePica_AvaTax
+ * @author     OnePica Codemaster <codemaster@onepica.com>
+ * @copyright  Copyright (c) 2009 One Pica, Inc.
+ * @license    http://opensource.org/licenses/osl-3.0.php Open Software License (OSL 3.0)
+ */
+
+/**
+ * The Onepage Shipping Method Available block
+ *
+ * @category   OnePica
+ * @package    OnePica_AvaTax
+ * @author     OnePica Codemaster <codemaster@onepica.com>
+ */
+class OnePica_AvaTax_Block_Checkout_Abstract extends Mage_Core_Block_Abstract
+{
+    /**
+     * Quote address
+     *
+     * @var Mage_Sales_Model_Quote_Address
+     */
+    protected $_address;
+
+    /**
+     * Checkout session
+     *
+     * @var Mage_Checkout_Model_Session
+     */
+    protected $_checkout;
+
+    /**
+     * Sales quote
+     *
+     * @var Mage_Sales_Model_Quote
+     */
+    protected $_quote;
+
+    /**
+     * config data
+     *
+     * @var OnePica_AvaTax_Helper_Config
+     */
+    protected $_configData;
+
+    /**
+     * Get quote address
+     *
+     * @return Mage_Sales_Model_Quote_Address
+     */
+    public function getAddress()
+    {
+        if (empty($this->_address)) {
+            $this->_address = $this->getQuote()->getShippingAddress();
+        }
+
+        return $this->_address;
+    }
+
+    /**
+     * Retrieve checkout session model
+     *
+     * @return Mage_Checkout_Model_Session
+     */
+    public function getCheckout()
+    {
+        if (empty($this->_checkout)) {
+            $this->_checkout = Mage::getSingleton('checkout/session');
+        }
+
+        return $this->_checkout;
+    }
+
+    /**
+     * Retrieve sales quote model
+     *
+     * @return Mage_Sales_Model_Quote
+     */
+    public function getQuote()
+    {
+        if (empty($this->_quote)) {
+            $this->_quote = $this->getCheckout()->getQuote();
+        }
+
+        return $this->_quote;
+    }
+}
--- app/code/community/OnePica/AvaTax/Block/Checkout/Onepage/Shipping/Method/Available.php
+++ app/code/community/OnePica/AvaTax/Block/Checkout/Onepage/Shipping/Method/Available.php
@@ -22,74 +22,14 @@
  * @package    OnePica_AvaTax
  * @author     OnePica Codemaster <codemaster@onepica.com>
  */
-class OnePica_AvaTax_Block_Checkout_Onepage_Shipping_Method_Available extends Mage_Core_Block_Abstract
+class OnePica_AvaTax_Block_Checkout_Onepage_Shipping_Method_Available extends OnePica_AvaTax_Block_Checkout_Abstract
 {
-    /**
-     * Quote address
-     *
-     * @var Mage_Sales_Model_Quote_Address
-     */
-    protected $_address;
-
-    /**
-     * Checkout session
-     *
-     * @var Mage_Checkout_Model_Session
-     */
-    protected $_checkout;
-
-    /**
-     * Sales quote
-     *
-     * @var Mage_Sales_Model_Quote
-     */
-    protected $_quote;
-
-    /**
-     * Get quote address
-     *
-     * @return Mage_Sales_Model_Quote_Address
-     */
-    public function getAddress()
-    {
-        if (empty($this->_address)) {
-            $this->_address = $this->getQuote()->getShippingAddress();
-        }
-        return $this->_address;
-    }
-
-    /**
-     * Retrieve checkout session model
-     *
-     * @return Mage_Checkout_Model_Session
-     */
-    public function getCheckout()
-    {
-        if (empty($this->_checkout)) {
-            $this->_checkout = Mage::getSingleton('checkout/session');
-        }
-        return $this->_checkout;
-    }
-
-    /**
-     * Retrieve sales quote model
-     *
-     * @return Mage_Sales_Model_Quote
-     */
-    public function getQuote()
-    {
-        if (empty($this->_quote)) {
-            $this->_quote = $this->getCheckout()->getQuote();
-        }
-        return $this->_quote;
-    }
-
     /**
      * Get config helper
      *
      * @return OnePica_AvaTax_Helper_Config
      */
-    private function _getConfigData()
+    protected function _getConfigData()
     {
         return Mage::helper('avatax/config');
     }
@@ -99,17 +39,14 @@ class OnePica_AvaTax_Block_Checkout_Onepage_Shipping_Method_Available extends Ma
      *
      * @return string
      */
-    protected function _toHtml ()
+    protected function _toHtml()
     {
         $additional = parent::_toHtml();
 
-        /** @var OnePica_AvaTax_Helper_Address $addressHelper */
-        $addressHelper = Mage::helper('avatax/address');
-        $quote = $this->getQuote();
         $normalizeAddressDisabler = $this->_getConfigData()->getNormalizeAddressDisabler();
         $checkboxDisabler = '';
         if ($normalizeAddressDisabler) {
-            $checkboxDisabler = $addressHelper->getOnepageDisableNormalizationCheckbox($quote->getAvataxNormalizationFlag());
+            $checkboxDisabler = Mage::getBlockSingleton('avatax/checkout_onepage_address_normalization_disabler')->toHtml();
         }
         if ($this->getAddress()->getAddressNormalized()) {
             $notice = Mage::helper('avatax/config')->getOnepageNormalizeMessage(Mage::app()->getStore());
@@ -127,8 +64,6 @@ class OnePica_AvaTax_Block_Checkout_Onepage_Shipping_Method_Available extends Ma
         ) {
             $additional .= $checkboxDisabler;
         }
-
-
 
         return $additional;
     }
