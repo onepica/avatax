6560dc194cac1c3477eaf6e6e3e7339599de2ea3
Oleksandr Marynych <o.marynych@astoundcommerce.com>
2017-07-14 20:44:09 +0300

    implemented AV-1616: Magento SOAP API Error + fixed regiter - key already exists

---
+++ app/code/community/OnePica/AvaTax/Model/Observer/SalesQuoteLoadAfter.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * OnePica_AvaTax
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Open Software License (OSL 3.0), a
+ * copy of which is available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/osl-3.0.php
+ *
+ * @category   OnePica
+ * @package    OnePica_AvaTax
+ * @author     OnePica Codemaster <codemaster@onepica.com>
+ * @copyright  Copyright (c) 2015 One Pica, Inc.
+ * @license    http://opensource.org/licenses/osl-3.0.php Open Software License (OSL 3.0)
+ */
+
+/**
+ * Avatax Observer SalesQuoteLoadAfter
+ *
+ * @category   OnePica
+ * @package    OnePica_AvaTax
+ * @author     OnePica Codemaster <codemaster@onepica.com>
+ */
+class OnePica_AvaTax_Model_Observer_SalesQuoteLoadAfter extends OnePica_AvaTax_Model_Observer_Abstract
+{
+    /**
+     * Register avatax_store_id for SOAP API set addresses
+     *
+     * @param Varien_Event_Observer $observer
+     * @return $this
+     */
+    public function execute(Varien_Event_Observer $observer)
+    {
+        $storeId = $observer->getEvent()->getQuote()->getStoreId();
+        if (Mage::registry('avatax_store_id')) {
+            Mage::unregister('avatax_store_id');
+        }
+        Mage::register('avatax_store_id', $storeId);
+        return $this;
+    }
+}
