316d03d69f44b8d37a794971aa09e03d4c4c9630
Zamoroka <zapashok0@gmail.com>
2017-09-29 11:22:37 +0300

    AV-1670: Related Data Interpretation

--- app/code/community/OnePica/AvaTax/Model/Records/Mysql4/Log/Collection.php
+++ app/code/community/OnePica/AvaTax/Model/Records/Mysql4/Log/Collection.php
@@ -35,65 +35,6 @@ class OnePica_AvaTax_Model_Records_Mysql4_Log_Collection extends Mage_Core_Model
         $this->_init('avatax_records/log');
     }
 
-    public function addRelatedInfoToSelect()
-    {
-
-        $this->getSelect()->joinLeft(
-            array('order_address' => $this->getTable('sales/order_address')),
-            'main_table.quote_address_id = order_address.avatax_quote_address_id',
-            array(
-                'order_address.entity_id' => 'entity_id',
-                'order_address.parent_id' => 'parent_id',
-            )
-        );
-        $this->getSelect()->joinLeft(
-            array('order' => $this->getTable('sales/order')),
-            'order_address.parent_id = order.entity_id',
-            array(
-                'order.entity_id' => 'entity_id',
-                'order.increment_id' => 'increment_id',
-            )
-        );
-        $this->getSelect()->joinLeft(
-            array('invoice' => $this->getTable('sales/invoice')),
-            'order.entity_id = invoice.order_id',
-            array(
-                'invoice.entity_id'    => 'entity_id',
-                'invoice.increment_id' => 'increment_id'
-            )
-        );
-        $this->getSelect()->joinLeft(
-            array('creditmemo' => $this->getTable('sales/creditmemo')),
-            'order.entity_id = creditmemo.order_id',
-            array(
-                'creditmemo.entity_id'    => 'entity_id',
-                'creditmemo.increment_id' => 'increment_id'
-            )
-        );
-
-        $this->_relatedInformationAdded = true;
-
-        return $this;
-    }
-
-    /**
-     *
-     */
-    public function interpretRelatedInformation()
-    {
-
-        /**
-         * Related Data Interpretation:
-         * SalesOrder id and incremental id should be present for log only if GetTaxRequest:DocType is SalesOrder
-         * SalesInvoice incremental id should be present for log only if GetTaxRequest:DocType is SalesInvoce
-         * CreditMemo incremental id should be present for log only if GetTaxRequest:DocType is ReturnInvoice
-         * interpretRelatedInformation content should be wrapped into try - catch block
-         */
-
-        //TODO: implement
-        return $this;
-    }
-
     /**
      * Redeclare after load method for specifying collection items original data
      *
@@ -107,6 +48,108 @@ class OnePica_AvaTax_Model_Records_Mysql4_Log_Collection extends Mage_Core_Model
         }
 
         return $this;
+    }
+
+    /**
+     * Add related info to select
+     *
+     * @return $this
+     */
+    public function addRelatedInfoToSelect()
+    {
+
+        $this->getSelect()->joinLeft(
+            array('order_address' => $this->getTable('sales/order_address')),
+            'main_table.quote_address_id = order_address.avatax_quote_address_id',
+            array(
+                'order_address_entity_id' => 'entity_id',
+                'order_address_parent_id' => 'parent_id',
+            )
+        );
+        $this->getSelect()->joinLeft(
+            array('order' => $this->getTable('sales/order')),
+            'order_address.parent_id = order.entity_id',
+            array(
+                'order_entity_id'    => 'entity_id',
+                'order_increment_id' => 'increment_id',
+            )
+        );
+        $this->getSelect()->joinLeft(
+            array('invoice' => $this->getTable('sales/invoice')),
+            'order.entity_id = invoice.order_id',
+            array(
+                'invoice_entity_id'    => 'entity_id',
+                'invoice_increment_id' => 'increment_id'
+            )
+        );
+        $this->getSelect()->joinLeft(
+            array('creditmemo' => $this->getTable('sales/creditmemo')),
+            'order.entity_id = creditmemo.order_id',
+            array(
+                'creditmemo_entity_id'    => 'entity_id',
+                'creditmemo_increment_id' => 'increment_id'
+            )
+        );
+
+        $this->_relatedInformationAdded = true;
+
+        return $this;
+    }
+
+    /**
+     * Related Data Interpretation
+     * SalesOrder id and incremental id should be present for log only if GetTaxRequest:DocType is SalesOrder
+     * SalesInvoice incremental id should be present for log only if GetTaxRequest:DocType is SalesInvoce
+     * CreditMemo incremental id should be present for log only if GetTaxRequest:DocType is ReturnInvoice
+     *
+     * @return $this
+     */
+    public function interpretRelatedInformation()
+    {
+        foreach ($this->getItems() as $item) {
+            $relatedData = new Varien_Object();
+            switch ($this->getRequestDocType($item)) {
+                case 'SalesOrder':
+                    $relatedData->setDocType('SalesOrder');
+                    $relatedData->setIncrementId($item->getOrderIncrementId());
+                    break;
+                case 'SalesInvoice':
+                    $relatedData->setDocType('SalesInvoice');
+                    $relatedData->setIncrementId($item->getInvoiceIncrementId());
+                    break;
+                case 'ReturnInvoice':
+                    $relatedData->setDocType('ReturnInvoice');
+                    $relatedData->setIncrementId($item->getCreditmemoIncrementId());
+                    break;
+                default:
+                    break;
+            }
+
+            $item->setRelatedData($relatedData);
+        }
+
+        return $this;
+    }
+
+    /**
+     * Regex to get DocType from GetTax request
+     *
+     * @param OnePica_AvaTax_Model_Records_Log $item
+     * @return string
+     */
+    public function getRequestDocType($item)
+    {
+        if ($item->getType() === 'GetTax') {
+            try {
+                preg_match("/\[DocType\:GetTaxRequest\:private\].\=\>.(.*)$/m", $item->getRequest(), $output);
+                if (array_key_exists(1, $output)) {
+                    return $output[1];
+                }
+            } catch (Exception $e) {
+            }
+        }
+
+        return false;
     }
 }
 
