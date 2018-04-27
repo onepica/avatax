83064b664be4dd7b792d3f892862f7ac5f651bcc
Oleksandr Marynych <o.marynych@astoundcommerce.com>
2018-04-27 15:52:52 +0300

    implemented AV-1794: Investigation of CR: Unit Of Mesurement

---
+++ app/code/community/OnePica/AvaTax/Model/Catalog/Product/Attribute/Backend/Unit.php
@@ -0,0 +1,92 @@
+<?php
+/**
+ * Magento Enterprise Edition
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Magento Enterprise Edition End User License Agreement
+ * that is bundled with this package in the file LICENSE_EE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://www.magento.com/license/enterprise-edition
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@magento.com so we can send you a copy immediately.
+ *
+ * DISCLAIMER
+ *
+ * Do not edit or add to this file if you wish to upgrade Magento to newer
+ * versions in the future. If you wish to customize Magento for your
+ * needs please refer to http://www.magento.com for more information.
+ *
+ * @category    Mage
+ * @package     Mage_Catalog
+ * @copyright Copyright (c) 2006-2017 X.commerce, Inc. and affiliates (http://www.magento.com)
+ * @license http://www.magento.com/license/enterprise-edition
+ */
+
+
+/**
+ * Catalog product price attribute backend model
+ *
+ * @category   Mage
+ * @package    Mage_Catalog
+ * @author     Magento Core Team <core@magentocommerce.com>
+ *
+ */
+class OnePica_AvaTax_Model_Catalog_Product_Attribute_Backend_Unit extends Mage_Eav_Model_Entity_Attribute_Backend_Abstract
+{
+    /**
+     * Set Attribute instance
+     * Rewrite for redefine attribute scope
+     *
+     * @param Mage_Catalog_Model_Resource_Eav_Attribute $attribute
+     * @return Mage_Catalog_Model_Product_Attribute_Backend_Price
+     */
+    public function setAttribute($attribute)
+    {
+        parent::setAttribute($attribute);
+        return $this;
+    }
+
+    /**
+     * After load method
+     *
+     * @param Varien_Object $object
+     * @return Mage_Eav_Model_Entity_Attribute_Backend_Abstract
+     */
+    public function afterLoad($object)
+    {
+        $attrCode = $this->getAttribute()->getAttributeCode();
+        if ($object->hasData($attrCode)) {
+            $config = json_decode($object->getData($attrCode));
+            $result = array();
+            if (!empty($config)) {
+                foreach ($config as $c) {
+                   array_push($result, (array)$c);
+                }
+            }
+
+            $object->setData($attrCode, $result);
+        }
+        return $this;
+    }
+
+    /**
+     * Before save method
+     *
+     * @param Varien_Object $object
+     * @return Mage_Eav_Model_Entity_Attribute_Backend_Abstract
+     */
+    public function beforeSave($object)
+    {
+        $attrCode = $this->getAttribute()->getAttributeCode();
+        if ($object->hasData($attrCode)) {
+            $config = json_encode($object->getData($attrCode));
+            $object->setData($attrCode, $config);
+        } else if (!$object->hasData($attrCode) && $this->getDefaultValue()) {
+            $object->setData($attrCode, $this->getDefaultValue());
+        }
+
+        return $this;
+    }
+}
