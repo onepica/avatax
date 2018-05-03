69d7e49df273e9a56a9596ea48a86b8aa420d236
Oleksandr Marynych <o.marynych@astoundcommerce.com>
2018-05-03 16:26:39 +0300

    implemented AV-1809: CR: refactoring - minor comments

--- app/code/community/OnePica/AvaTax/sql/avatax_records_setup/mysql4-upgrade-3.5.7.5-3.5.7.6.php
+++ app/code/community/OnePica/AvaTax/sql/avatax_records_setup/mysql4-upgrade-3.5.7.5-3.5.7.6.php
@@ -36,7 +36,7 @@ if ($installer->getConnection()->isTableExists($table)) {
         // no United States of America, Liberia, Myanmar
         $allCountriesExceptFew = 'AD,AE,AF,AG,AI,AL,AM,AN,AO,AQ,AR,AS,AT,AU,AW,AX,AZ,BA,BB,BD,BE,BF,BG,BH,BI,BJ,BL,BM,BN,BO,BR,BS,BT,BV,BW,BY,BZ,CA,CC,CD,CF,CG,CH,CI,CK,CL,CM,CN,CO,CR,CU,CV,CX,CY,CZ,DE,DJ,DK,DM,DO,DZ,EC,EE,EG,EH,ER,ES,ET,FI,FJ,FK,FM,FO,FR,GA,GB,GD,GE,GF,GG,GH,GI,GL,GM,GN,GP,GQ,GR,GS,GT,GU,GW,GY,HK,HM,HN,HR,HT,HU,ID,IE,IL,IM,IN,IO,IQ,IR,IS,IT,JE,JM,JO,JP,KE,KG,KH,KI,KM,KN,KP,KR,KW,KY,KZ,LA,LB,LC,LI,LK,LS,LT,LU,LV,LY,MA,MC,MD,ME,MF,MG,MH,MK,ML,MN,MO,MP,MQ,MR,MS,MT,MU,MV,MW,MX,MY,MZ,NA,NC,NE,NF,NG,NI,NL,NO,NP,NR,NU,NZ,OM,PA,PE,PF,PG,PH,PK,PL,PM,PN,PR,PS,PT,PW,PY,QA,RE,RO,RS,RU,RW,SA,SB,SC,SD,SE,SG,SH,SI,SJ,SK,SL,SM,SN,SO,SR,ST,SV,SY,SZ,TC,TD,TF,TG,TH,TJ,TK,TL,TM,TN,TO,TR,TT,TV,TW,TZ,UA,UG,UM,UY,UZ,VA,VC,VE,VG,VI,VN,VU,WF,WS,YE,YT,ZA,ZM,ZW';
 
-        // add Zend_Measure_Weight::KILOGRAM
+        // add Kilogram
         {
             /** @var $unit OnePica_AvaTax_Model_Records_UnitOfMeasurement */
             $unit = Mage::getModel('avatax_records/unitOfMeasurement');
@@ -48,7 +48,7 @@ if ($installer->getConnection()->isTableExists($table)) {
             $collection->addItem($unit);
         }
 
-        // add Zend_Measure_Weight::GRAM
+        // add Gram
         {
             /** @var $unit OnePica_AvaTax_Model_Records_UnitOfMeasurement */
             $unit = Mage::getModel('avatax_records/unitOfMeasurement');
