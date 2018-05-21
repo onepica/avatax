ea974491c8332999b437a710af46b2c7e4f89c50
Pavlo Zamoroka <p.zamoroka@astoundcommerce.com>
2018-05-21 12:26:19 +0300

    AV-1828: show companies as dropdown

---
+++ app/code/community/OnePica/AvaTax/Model/Source/Avatax/Companies.php
@@ -0,0 +1,84 @@
+<?php
+/**
+ * OnePica_AvaTax
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Open Software License (OSL 3.0), a
+ * copy of which is available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/osl-3.0.php
+ *
+ * @category   OnePica
+ * @package    OnePica_AvaTax
+ * @author     OnePica Codemaster <codemaster@onepica.com>
+ * @copyright  Copyright (c) 2009 One Pica, Inc.
+ * @license    http://opensource.org/licenses/osl-3.0.php Open Software License (OSL 3.0)
+ */
+
+/**
+ * @class      OnePica_AvaTax_Model_Source_Avatax_Actions
+ * Actions source model
+ *
+ * @category   OnePica
+ * @package    OnePica_AvaTax
+ * @author     OnePica Codemaster <codemaster@onepica.com>
+ */
+class OnePica_AvaTax_Model_Source_Avatax_Companies
+{
+    /**
+     * Gets the list of cache methods for the admin config dropdown
+     *
+     * @return array
+     */
+    public function toArray()
+    {
+        $result = array(
+            array(
+                'value' => '',
+                'label' => ''
+            )
+        );
+
+        /** @var \stdClass $company */
+        foreach ($this->_getCompanies() as $company) {
+            $result[] = array(
+                'value' => $company->CompanyCode,
+                'label' => $company->CompanyName
+            );
+        }
+
+        return $result;
+    }
+
+    /**
+     * Get companies for account
+     *
+     * @return array
+     */
+    protected function _getCompanies()
+    {
+        $companies = $this->_getServiceConfig()->getAccountConnection()->CompanyFetch('')->getValidCompanies();
+
+        return (array)$companies;
+    }
+
+    /**
+     * Get config helper
+     *
+     * @return OnePica_AvaTax_Helper_Config
+     */
+    protected function _getConfigHelper()
+    {
+        return Mage::helper('avatax/config');
+    }
+
+    /**
+     * Get Service Config
+     *
+     * @return OnePica_AvaTax_Model_Service_Avatax_Config
+     */
+    protected function _getServiceConfig()
+    {
+        return Mage::getModel('avatax/service_avatax_config')->init(Mage::app()->getStore());
+    }
+}
