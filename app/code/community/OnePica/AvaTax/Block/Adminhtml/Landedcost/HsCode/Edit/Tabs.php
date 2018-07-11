5b2a3534bbd641ec1029d985365abba170ee71a5
Pavlo Zamoroka <p.zamoroka@astoundcommerce.com>
2018-07-11 14:14:16 +0300

    AV-1946: Rename HS code label

--- app/code/community/OnePica/AvaTax/Block/Adminhtml/Landedcost/HsCode/Edit/Tabs.php
+++ app/code/community/OnePica/AvaTax/Block/Adminhtml/Landedcost/HsCode/Edit/Tabs.php
@@ -7,7 +7,7 @@ class OnePica_AvaTax_Block_Adminhtml_Landedcost_HsCode_Edit_Tabs extends Mage_Ad
         parent::__construct();
         $this->setId('hscode_tabs');
         $this->setDestElementId('edit_form');
-        $this->setTitle($this->__('HS Code'));
+        $this->setTitle($this->__('HS Code Group'));
     }
 
     /**
@@ -18,8 +18,8 @@ class OnePica_AvaTax_Block_Adminhtml_Landedcost_HsCode_Edit_Tabs extends Mage_Ad
     {
         $this->addTab(
             'form_section', array(
-                'label'   => $this->__('HS code information'),
-                'title'   => $this->__('HS code information'),
+                'label'   => $this->__('HS Code Group Information'),
+                'title'   => $this->__('HS Code Group Information'),
                 'content' => $this->getLayout()->createBlock('avatax/adminhtml_landedcost_hsCode_edit_tab_form')
                                   ->toHtml(),
             )
