1b86960ab9ba50f49eeec18a0177000c03d8fd05
Pavlo Zamoroka <p.zamoroka@astoundcommerce.com>
2018-07-12 16:42:26 +0300

    AV-1940: integrate into tab [create new certificate] button

---
+++ app/code/community/OnePica/AvaTaxAr2/Block/Documents/Grid/Button.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * OnePica_AvaTax
+ * NOTICE OF LICENSE
+ * This source file is subject to the Open Software License (OSL 3.0), a
+ * copy of which is available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/osl-3.0.php
+ *
+ * @category   OnePica
+ * @package    OnePica_AvaTax
+ * @author     OnePica Codemaster <codemaster@onepica.com>
+ * @copyright  Copyright (c) 2009 One Pica, Inc.
+ * @license    http://opensource.org/licenses/osl-3.0.php Open Software License (OSL 3.0)
+ */
+
+/**
+ * Class OnePica_AvaTaxAr2_Block_Documents_Grid_Form
+ */
+class OnePica_AvaTaxAr2_Block_Documents_Grid_Button extends Mage_Core_Block_Template
+{
+    /**
+     * @return string
+     */
+    public function getPopupUrl()
+    {
+        return $this->getUrl('avataxcert/popup/genCert', array('customerNumber' => $this->getCustomerNumber()));
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getCustomerNumber()
+    {
+        return $this->_getSession()->getCustomer()->getData(OnePica_AvaTaxAr2_Helper_Data::AVATAX_CUSTOMER_CODE);
+    }
+
+    /**
+     * @return \Mage_Customer_Model_Session
+     */
+    protected function _getSession()
+    {
+        return Mage::getSingleton('customer/session');
+    }
+}
